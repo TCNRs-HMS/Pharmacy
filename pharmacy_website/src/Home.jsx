403c468101fb3fa2716b8a8f03b663d0a6e53ee3
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-11-28 14:04:11 +0530

    Home Page
    Home Page was created of the pharmacy website.

---
+++ pharmacy_website/src/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <main>
+        <div className='container'>
+            <h3>Welcome</h3>
+            <h1>CALISTO MEDILAB</h1>
+            <h4>THE ONLINE PHARMACEUTICAL SHOP</h4>
+        </div>
+    </main>
+  )
+}
+
+export default Home
