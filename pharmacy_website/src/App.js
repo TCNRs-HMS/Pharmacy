72ff608dbee250b97cd377fdb9a0d0f8be988675
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-12-05 14:43:54 +0530

    Header
    Header of the website was created as a component.

--- pharmacy_website/src/App.js
+++ pharmacy_website/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Home from './Home';
-import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <Home />      
+    <div className='grid-container'>
+      <Home/>      
+      <Header/>
     </div>
   );
 }
