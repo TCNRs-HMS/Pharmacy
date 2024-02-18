692298504fcfae0f37905aaa9b06f987d0d73d87
Teshan2000 <teshan_wijewardhana@outlook.com>
2024-02-18 17:16:40 +0530

    Header Component
    Header was connected the home page.

--- pages/Contact.php
+++ pages/Contact.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="pharmacy, medicine, tablets, pills, doctors, diseases, e-channelling">
-    <meta name="description" content="This website helps people to buy medicines via online easily.">
-    <link href="css/bootstrap.min.css" rel="stylesheet">   
+    <meta name="description" content="This website helps people to buy medicines via online easily.">  
     <link href="css/styles.css" rel="stylesheet">   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Oswald:wght@500;600;700&family=Pacifico&display=swap" rel="stylesheet"> 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
--- pages/Home.php
+++ pages/Home.php
@@ -1,67 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="pharmacy, medicine, tablets, pills, doctors, diseases, e-channelling">
-    <meta name="description" content="This website helps people to buy medicines via online easily.">
-    <link href="css/bootstrap.min.css" rel="stylesheet">   
-    <link href="css/styles.css" rel="stylesheet">   
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Oswald:wght@500;600;700&family=Pacifico&display=swap" rel="stylesheet"> 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-    <title>Calisto Medilab | Home</title>
-</head>
-<body>
-    <div class="container-fluid bg-primary p-3">
-        <div class="row">
-            <div class="col-lg-10 px-4">
-                <h1 class="text-uppercase text-light"><i class="fa fa-medkit text-light"></i>&nbsp;Calisto Medilab</h2>
-            </div>
-            <div class="col-lg-1 text-center">
-                <i class="fa fa-user fs-3"></i><br>
-                <span class="text-light">Account</span>
-            </div>
-            <div class="col-lg-1 text-center">
-                <i class="fa fa-shopping-cart fs-3"></i><br>
-                <span class="text-light">Cart</span>                    
-            </div>
-        </div>
-    </div>
-
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-3 pb-5">
-        <ul class="navbar-nav me-auto me-lg-auto py-0">  
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link active dropdown-toggle" data-bs-toggle="dropdown">Categories</a>
-                    <ul class="dropdown-menu">                       
-                        <li><a href="Categories/vitemins.html" class="nav-item nav-link">Vitamin & Suppliments</a></li>
-                        <li><a href="Categories/minstrument.html" class="nav-item nav-link">Medical Instruments</a></li>
-                        <li><a href="Categories/mother&baby.html" class="nav-item nav-link" target="_self">Mother & Baby care</a></li>
-                        <li><a href="Categories/pet.html" class="nav-item nav-link" target="_self">Pet care</a></li>
-                        <li><a href="Categories/dental.html" class="nav-item nav-link" target="_self">Dental care</a></li>
-                        <li><a href="Categories/skin.html" class="nav-item nav-link" target="_self">Skin care</a></li>
-                        <li><a href="Categories/hair.html" class="nav-item nav-link" target="_self">Hair care</a></li>
-                        <li><a href="Categories/eye.html" class="nav-item nav-link" target="_self">Eye care</a></li>
-                        <li><a href="Categories/intimacy.html" class="nav-item nav-link" target="_self">Intimacy</a></li>
-                        <li><a href="Categories/mdevices.html" class="nav-item nav-link" target="_self">Medical devices</a></li>
-                    </ul>
-                </li>  
-            </ul>          
-        <ul class="navbar-nav mx-auto">
-            <li><a href="#" class="nav-item nav-link active">Home</a></li>
-            <li><a href="#" class="nav-item nav-link">Menu</a></li>
-            <li><a href="#" class="nav-item nav-link">Our Services</a></li>
-            <li><a href="#" class="nav-item nav-link">Contact Us</a></li>
-            <li><a href="#" class="nav-item nav-link">About Us</a></li>
-            <li><a href="#" class="nav-item nav-link">Testimonial</a></li>
-         </ul>        
-        <div class="">
-            <form>
-                <input type="text" placeholder="search">
-                <button type="submit"><i class="fa fa-search"></i></button>
-            </form>         
-        </div>        
-    </nav><br><br>
+<?php include("./Header.php") ?>
     
     <div class="container-fluid bg-light about py-5">
         <div class="container">
@@ -70,7 +7,6 @@
                 <hr style="width:100%; height:4px">
             </div>
 
-            <!--Back wenna puluwn කොටස  Home/medical-->
             <div class="row">
                 <div class="col-5 text-left">
                     <a href="Home page.html">Home</a>
@@ -220,6 +156,3 @@
         </div>
     </div>
 
-    
-</body>
-</html>
