539407a79cbdd6124a8ce606f355ac094591c7e9
Teshan2000 <teshan_wijewardhana@outlook.com>
2024-02-21 15:02:43 +0530

    Contact Us Updated
    Contact us page was updated with header & footer.

--- pages/Contact.php
+++ pages/Contact.php
@@ -1,69 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="pharmacy, medicine, tablets, pills, doctors, diseases, e-channelling">
-    <meta name="description" content="This website helps people to buy medicines via online easily.">  
-    <link href="css/styles.css" rel="stylesheet">   
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Oswald:wght@500;600;700&family=Pacifico&display=swap" rel="stylesheet"> 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-    <title>Calisto Medilab | Contact Us</title>
-</head>
-<body>
-    <div class="container-fluid bg-primary p-3">
-        <div class="row">
-            <div class="col-lg-10 px-4">
-                <h1 class="text-uppercase text-light"><i class="fa fa-medkit text-light"></i>&nbsp;Calisto Medilab</h2>
-            </div>
-            <div class="col-lg-1 text-center">
-                <i class="fa fa-user fs-3"></i><br>
-                <span class="text-light">Account</span>
-            </div>
-            <div class="col-lg-1 text-center">
-                <i class="fa fa-shopping-cart fs-3"></i><br>
-                <span class="text-light">Cart</span>                    
-            </div>
-        </div>
-    </div>
+<?php include("./Header.php") ?>
 
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-3 pb-3">
-        <ul class="navbar-nav me-auto me-lg-auto py-0">  
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link active dropdown-toggle" data-bs-toggle="dropdown">Categories</a>
-                    <ul class="dropdown-menu">                       
-                        <li><a href="Categories/vitemins.html" class="nav-item nav-link">Vitamin & Suppliments</a></li>
-                        <li><a href="Categories/minstrument.html" class="nav-item nav-link">Medical Instruments</a></li>
-                        <li><a href="Categories/mother&baby.html" class="nav-item nav-link" target="_self">Mother & Baby care</a></li>
-                        <li><a href="Categories/pet.html" class="nav-item nav-link" target="_self">Pet care</a></li>
-                        <li><a href="Categories/dental.html" class="nav-item nav-link" target="_self">Dental care</a></li>
-                        <li><a href="Categories/skin.html" class="nav-item nav-link" target="_self">Skin care</a></li>
-                        <li><a href="Categories/hair.html" class="nav-item nav-link" target="_self">Hair care</a></li>
-                        <li><a href="Categories/eye.html" class="nav-item nav-link" target="_self">Eye care</a></li>
-                        <li><a href="Categories/intimacy.html" class="nav-item nav-link" target="_self">Intimacy</a></li>
-                        <li><a href="Categories/mdevices.html" class="nav-item nav-link" target="_self">Medical devices</a></li>
-                    </ul>
-                </li>  
-            </ul>          
-        <ul class="navbar-nav mx-auto">
-            <li><a href="Home.html" class="nav-item nav-link active">Home</a></li>
-            <li><a href="#" class="nav-item nav-link">Menu</a></li>
-            <li><a href="#" class="nav-item nav-link">Our Services</a></li>
-            <li><a href="Contact.html" class="nav-item nav-link">Contact Us</a></li>
-            <li><a href="#" class="nav-item nav-link">About Us</a></li>
-            <li><a href="#" class="nav-item nav-link">Testimonial</a></li>
-         </ul>        
-        <div class="">
-            <form>
-                <input type="text" placeholder="search">
-                <button type="submit"><i class="fa fa-search"></i></button>
-            </form>         
-        </div>        
-    </nav><br>
-    
     <div class="container-fluid bg-dark pt-5">
         <div class="row text-center">
             <div class="col-12">
@@ -125,7 +61,7 @@
             </div>
             <div class="col-lg-7 text-center">
                 <div class="bg-primary p-3">
-                    <img src="img/location.png" height="410" class="w-100" alt="location">
+                    <img src="/img/location.png" height="410" class="w-100" alt="location">
                 </div>
             </div>
         </div><br><br>
@@ -155,68 +91,5 @@
         </div>
     </div><br><br><br>
 
-    <div class="container-fluid bg-dark pt-5">
-        <div class="row">
-            <div class="col-lg-2 text-center">
-                <ul class="text-start"><h4 class="text-primary text-uppercase">Overview</h4>
-                    <a href="#" class="nav-item nav-link active text-light">Home</a>
-                    <a href="#" class="nav-item nav-link text-light">Menu</a>
-                    <a href="#" class="nav-item nav-link text-light">Our Services</a>
-                    <a href="#" class="nav-item nav-link text-light">Contact Us</a>
-                    <a href="#" class="nav-item nav-link text-light">About Us</a>
-                    <a href="#" class="nav-item nav-link text-light">Testimonial</a>
-                </ul>
-            </div>
-            <div class="col-lg-3 text-center">
-                <ul class="text-start"><h4 class="text-primary text-uppercase">Categories</h4>
-                    <a href="Categories/vitemins.html" class="nav-item nav-link text-light">Vitamin & Suppliments</a>
-                    <a href="Categories/minstrument.html" class="nav-item nav-link text-light">Medical Instruments</a>
-                    <a href="Categories/mother&baby.html" class="nav-item nav-link text-light">Mother & Baby care</a>
-                    <a href="Categories/pet.html" class="nav-item nav-link text-light">Pet care</a>
-                    <a href="Categories/dental.html" class="nav-item nav-link text-light">Dental care</a>
-                    <a href="Categories/skin.html" class="nav-item nav-link text-light">Skin care</a>
-                    <a href="Categories/hair.html" class="nav-item nav-link text-light">Hair care</a>
-                    <a href="Categories/eye.html" class="nav-item nav-link text-light">Eye care</a>
-                    <a href="Categories/intimacy.html" class="nav-item nav-link text-light">Intimacy</a>
-                    <a href="Categories/mdevices.html" class="nav-item nav-link text-light">Medical devices</a>
-                </ul>
-            </div>
-            <div class="col-lg-4">
-                <ul class="text-start"><h4 class="text-primary text-uppercase text-center">Contact Us</h4>
-                    <a href="#" class="nav-item nav-link text-light"><i class="bi bi-clock mx-1"></i>Opening Hours : 9.00 a.m - 9.00 p.m</a>
-                    <a href="#" class="nav-item nav-link text-light"><i class="bi bi-geo-alt mx-1"></i>No.65/A, Kandy Road, Badulla, Sri Lanka</a>
-                    <a href="#" class="nav-item nav-link text-light"><i class="bi bi-envelope mx-1"></i>calistopharmacy2019@gmail.com</a>
-                    <a href="#" class="nav-item nav-link text-light"><i class="bi bi-telephone mx-1"></i>+94 76 555 1064</a>                   
-                </ul>
-            </div>
-            <div class="col-lg-3 text-center">
-                <ul><h4 class="text-primary text-uppercase">Follow us</h4>
-                    <span class="nav-item nav-link text-light">Send Us Your Order</span>
-                    <form class="pb-4 pt-2">
-                        <input type="text" placeholder="Enter Email">
-                        <button type="submit"><i class="fab fa-telegram text-dark"></i></button>
-                    </form>
-                    <div class="flex-row">
-                        <a href="www.whatsapp.com" class="btn btn-lg btn-primary text-light mx-1"><i class="fab fa-whatsapp"></i></a>
-                        <a href="www.facebook.com" class="btn btn-lg btn-primary text-light mx-1"><i class="fab fa-facebook-f"></i></a>
-                        <a href="www.linkedin.com" class="btn btn-lg btn-primary text-light mx-1"><i class="fab fa-linkedin-in"></i></a>                    
-                    </div>                    
-                </ul>
-            </div>
-        </div>
-    </div>
-
-    <div class="container-fluid bg-dark pt-4 pb-4 border-top border-light">
-        <div class="row">
-            <div class="col-lg-6 text-start ps-5">
-                <span class="text-light">&copy;<a href="#" class="text-primary"> Calisto Medilab. </a>All Rights Reserved.</span>
-            </div>
-            <div class="col-lg-6 text-end pe-5">
-                <span class="text-light">Designed By<a href="#" class="text-primary"> Team-11-Eleven</a></span>
-            </div>
-        </div>
-    </div>
-
     
-</body>
-</html>
+<?php include("./Footer.php") ?>
